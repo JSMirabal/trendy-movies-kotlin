abc118efcfe6c68228266ed3e6acea15739845a8
jsmirabal <jmirabal15k@gmail.com>
2017-12-28 01:15:01 -0500

    Add bottom navigation view

--- app/src/main/java/com/japps/trendymovieskotlin/MainActivity.kt
+++ app/src/main/java/com/japps/trendymovieskotlin/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.japps.trendymovieskotlin
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toast
+import com.japps.trendymovieskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
+        binding.bottomNavigation.menu.findItem(R.id.nav_trending).isChecked = true
+        binding.executePendingBindings()
+    }
+
+    fun onBottomNavigationItemClick(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_trending -> Toast.makeText(this, "Trending", Toast.LENGTH_SHORT).show()
+            R.id.nav_top_rated -> Toast.makeText(this, "Top Rated", Toast.LENGTH_SHORT).show()
+            R.id.nav_upcoming -> Toast.makeText(this, "Upcoming", Toast.LENGTH_SHORT).show()
+            R.id.nav_now_playing -> Toast.makeText(this, "Now Playing", Toast.LENGTH_SHORT).show()
+            R.id.nav_revenue -> Toast.makeText(this, "Revenue", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
