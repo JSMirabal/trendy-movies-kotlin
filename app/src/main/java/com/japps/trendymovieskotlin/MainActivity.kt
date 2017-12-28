f1c257929bce5ff725a625af87c665f9832cf406
jsmirabal <julio.mirabal@globant.com>
2017-12-28 18:20:50 -0500

    Create card view item

--- app/src/main/java/com/japps/trendymovieskotlin/MainActivity.kt
+++ app/src/main/java/com/japps/trendymovieskotlin/MainActivity.kt
@@ -9,12 +9,18 @@ import com.japps.trendymovieskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.activity = this
-        binding.bottomNavigation.menu.findItem(R.id.nav_trending).isChecked = true
-        binding.executePendingBindings()
+        setupBinding()
+    }
+
+    private fun setupBinding() {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mBinding.self = this
+        mBinding.bottomNavigation.menu.findItem(R.id.nav_trending).isChecked = true
+        mBinding.executePendingBindings()
     }
 
     fun onBottomNavigationItemClick(item: MenuItem): Boolean {
